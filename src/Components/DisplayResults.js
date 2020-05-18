11ff8325260f2bb8f9673aac7cbf448b078ce906
Brian Holt <bharrisonholt@gmail.com>
2020-05-18 10:17:21 -0700

    fixed empty word list

--- src/Components/DisplayResults.js
+++ src/Components/DisplayResults.js
@@ -13,11 +13,10 @@ class Results extends Component {
 
         //prevents infinite loop
         if (this.props.word !== prevProps.word || this.props.typeSearch !== prevProps.typeSearch){
-        console.log(this.props.word, this.props.typeSearch)
+        
         let baseUrl = 'https://api.datamuse.com/words';
         let wordToSearch = this.props.word;
         let type;
-
         //determines what type of search to perform
         if (this.props.typeSearch === 'syn') {
             type = '?rel_syn='
@@ -25,7 +24,6 @@ class Results extends Component {
         if (this.props.typeSearch === 'ant') {
             type = '?rel_ant='
         }
-
         let query = baseUrl + type + wordToSearch;
         fetch(query)
             .then(res => res.json())
@@ -33,6 +31,7 @@ class Results extends Component {
                 (result) => {
                     //top ten results
                     let trimmedList = result.slice(0,10)
+                    console.log(trimmedList)
                     this.setState({
                         isLoaded: true,
                         wordList: trimmedList,
@@ -53,11 +52,13 @@ class Results extends Component {
         if (this.props.readyToFind) {
             const { error, isLoaded, wordList } = this.state;
             if (!this.props.word || !this.props.typeSearch){
-                return <div className='text-center' style={styles.list}><p>Uh oh something went wrong. Make sure you enter a word</p></div>
+                return <div className='text-center' style={styles.list}><p>Uh oh something went wrong. Make sure you enter a word and selected a button</p></div>
             } else if (error) {
                 return <div className='text-center' style={styles.list}>Error: {error.message}</div>
             } else if (!isLoaded) {
                 return <div className='text-center' style={styles.list}>Loading...</div>;
+            } else if (this.state.wordList.length < 1){
+                return <div className='text-center'>No words were found. Try again.</div>
             } else {
                 return (
                     <div className='col-9 col-md-6 mx-auto text-center mt-3' style={styles.list}>
